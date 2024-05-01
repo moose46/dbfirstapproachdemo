a7818a5d6bcbbd1e3c1b7b10b070b5701ea6c0d7
me <me@robertcurtiss.com>
2024-05-01 19:26:14 -0400

    31-104 Executing Raw SQL using Django ORM

--- dbfirstapproachapp/views.py
+++ dbfirstapproachapp/views.py
@@ -1,3 +1,4 @@
+import pyodbc
 from django.shortcuts import render
 
 from dbfirstapproachapp.models import Categories
@@ -10,3 +11,32 @@ def ShowCategories(request):
     categories = Categories.objects.all()
 
     return render(request, "dbfa/ShowCategories.html", {"Categories": categories})
+
+
+def RawSqlDemo(request):
+    query = """
+		  SELECT a.OrderID,a.OrderDate,b.CompanyName,
+		  c.ProductName,d.UnitPrice,d.Quantity,
+		  d.unitprice * d.Quantity as 'BillAmount'
+		  from orders a inner join [order details] d on a.orderid=d.orderid inner join
+		  customers b on a.customerid=b.customerid inner join
+		  products c on d.productid=c.productid
+		  where a.orderid between 10248 and 10255
+		  """
+    cnxn = GetConnection()
+    cursor = cnxn.cursor()
+    cursor.execute(query)
+    orders = cursor.fetchall()
+    return render(request, "dbfa/ShowOrders.html", {"Orders": orders})
+
+
+def GetConnection():
+    cnxn = ""
+    try:
+        cnxn = pyodbc.connect(
+            "Driver=ODBC Driver 17 for SQL Server;Server=.;Database=Northwind;Trusted_Connection=Yes;"
+        )
+        return cnxn
+    except Exception as e:
+        print(f"GetConnection: {e}")
+        return cnxn
--- dbfirstapproachdemo/urls.py
+++ dbfirstapproachdemo/urls.py
@@ -24,4 +24,5 @@ from dbfirstapproachapp import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("ShowCategories/", views.ShowCategories),
+    path("ShowOrders/", views.RawSqlDemo),
 ]
